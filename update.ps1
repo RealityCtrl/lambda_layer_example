b5a51d62e5afba1eaa144ad713494eaa7616a8b3
RealityCtrl <david.mccandless@outlook.com>
2018-12-12 16:05:23 +0000

    paramaterizing runtimes

---
+++ update.ps1
@@ -0,0 +1,6 @@
+Compress-Archive -Path C:\Users\david\git\sqs_helper_layer\sqs_helper.py -DestinationPath C:\tmp\code.zip
+Import-Module AWSPowerShell
+Write-S3Object -BucketName lambda-deployment-realityctrl-sndbx -File C:\tmp\code.zip
+$content = [IO.File]::ReadAllText("C:\Users\david\git\sqs_helper_layer\layer.yaml")
+$JSONFile = ConvertFrom-Json "$(get-content $(Join-Path C:\Users\david\git\sqs_helper_layer "deploy_params.json"))"
+Update-CFNStack -StackName mysqshelperlayerstack -TemplateBody $content -Parameter $JSONFile
